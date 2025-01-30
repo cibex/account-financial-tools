9a4583df3194ccbd13e2e28549fa553a0ee7899e
Carlos Lopez <carlos.lopez@tecnativa.com>
2025-01-30 11:50:40 -0500

    [FIX] account_journal_restrict_mode: Make compatible with other modules
    When this module is installed, it prevents the cancellation of posted journals. However, in many cases, this action is necessary—for example, in the account_asset_management module.
    
    With this commit, the default constraint is applied only in this module's tests or when Odoo is not running in test mode.
    
    This approach provides an alternative to avoid marking the module as rebellious.

--- account_journal_restrict_mode/models/account_journal.py
+++ account_journal_restrict_mode/models/account_journal.py
@@ -3,18 +3,34 @@
 
 from odoo import api, fields, models
 from odoo.exceptions import UserError
+from odoo.tools import config
 
 
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    restrict_mode_hash_table = fields.Boolean(default=True, readonly=True)
+    restrict_mode_hash_table = fields.Boolean(
+        default=lambda self: self._default_restrict_mode_hash_table(), readonly=True
+    )
 
     @api.constrains("restrict_mode_hash_table")
     def _check_journal_restrict_mode(self):
+        test_condition = not config["test_enable"] or (
+            config["test_enable"]
+            and self.env.context.get("test_account_journal_restrict_mode")
+        )
+        if not test_condition:
+            return
         for rec in self:
             if not rec.restrict_mode_hash_table:
                 raise UserError(
                     self.env._("Journal %s must have Lock Posted Entries enabled.")
                     % rec.name
                 )
+
+    def _default_restrict_mode_hash_table(self):
+        test_condition = not config["test_enable"] or (
+            config["test_enable"]
+            and self.env.context.get("test_account_journal_restrict_mode")
+        )
+        return bool(test_condition)
